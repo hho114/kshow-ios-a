dafc262a05dfb4cd0b4a6cc95df33f61de489c23
Alex Ho <huyho114@gmail.com>
2021-05-12 13:51:37 -0400

    first commit

---
+++ KSHOW/KSHOWApp.swift
@@ -0,0 +1,17 @@
+//
+//  KSHOWApp.swift
+//  KSHOW
+//
+//  Created by Alex Ho on 5/12/21.
+//
+
+import SwiftUI
+
+@main
+struct KSHOWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# kshow-ios-a
